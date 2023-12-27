ed9c3701bdf891ecca5ac32d71c8576c87bcbf12
chriseshun <eshunchris12@gmail.com>
2023-12-27 08:44:40 -0800

    working on import

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,9 +2,9 @@
 
 from add_0 import add
 
+
 if __name__ == "__main__":
+
     a = 1
     b = 2
-
-    result = add(a, b)
-    print(f"{a} + {b} = {result}")
+    print("{} + {} = {}".format(a, b, add(a, b)))
